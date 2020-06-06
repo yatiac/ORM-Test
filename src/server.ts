8f04caf22eb590578263df01f0ed68204aaa326b
Alois Carrera <aloiscarrera@gmail.com>
2020-06-06 16:07:39 -0500

    Added body-parser
    No recuerdo exactamente para qué se usa. Leyendo (según lo que entendí) funciona para convertir un objeto JSON que se envíe en una request a un objeto de JS para operar con él.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 import users from "./routes/users.routes";
 import index from "./routes/index.routes";
@@ -13,6 +14,7 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(bodyParser.json());
 
 app.use(index);
 app.use("/users", users);
