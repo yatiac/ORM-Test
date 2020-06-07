96ab00da0181042d6cacbbf0afad4aa1008b829f
Alois Carrera <aloiscarrera@gmail.com>
2020-06-07 12:19:44 -0500

    Knex config file

---
+++ knexfile.ts
@@ -0,0 +1,22 @@
+// Update with your config settings.
+require('ts-node/register')
+
+require('dotenv').config()
+
+module.exports = {
+
+  development: {
+    client: "postgresql",
+    connection: process.env.DB_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/development'
+    }
+  }
+};
