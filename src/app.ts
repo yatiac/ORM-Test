e9d5b1548200d75676e8df704572c49365aab814
Alois Carrera <aloiscarrera@gmail.com>
2020-06-07 15:07:57 -0500

    Server structure

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import Knex from "knex";
+import { Model } from "objection";
+
+const connection = require('../knexfile')
+import Users from './routes/user.routes'
+
+const knex = Knex(connection.development);
+
+Model.knex(knex);
+
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.use('/users', Users)
+
+export default app;
