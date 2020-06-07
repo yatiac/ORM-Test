d8cdae9a8a36bbcb42fd90714220677d94a9fbc6
Alois Carrera <aloiscarrera@gmail.com>
2020-06-07 17:12:06 -0500

    Relational endpoint
    Aquí se responde de una vez con la relación entre tablas, está increíble, se puede hacer más complejo y devolver lo que sea, literalmente.

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -4,7 +4,16 @@ import Users from "../models/user";
 
 export async function getAllUsers(_: Request, res: Response) {
 	const users = await Users.query();
-    res.json(users);
-    
-    return;
+	res.json(users);
+
+	return;
+}
+
+export async function getUserAndPosts(req: Request, res: Response) {
+	const userAndPosts = await Users.query()
+		.withGraphJoined('posts')
+		.findById(req.params.id);
+	res.json(userAndPosts);
+
+	return;
 }
