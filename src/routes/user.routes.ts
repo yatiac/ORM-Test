79f56f52beabc76d62939bbbeb1142e26f891375
Alois Carrera <aloiscarrera@gmail.com>
2020-06-07 17:14:09 -0500

    Retrieve user and its posts

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -5,5 +5,6 @@ import * as db from "../controllers/users.controller";
 const router = Router();
 
 router.route("/").get(db.getAllUsers);
+router.route('/:id').get(db.getUserAndPosts)
 
 export default router;
