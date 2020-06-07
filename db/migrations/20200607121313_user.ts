42d767d1a6253d7138c3ac9453cbab4a4fa4d615
Alois Carrera <aloiscarrera@gmail.com>
2020-06-07 15:04:03 -0500

    Users migration
    Creación de la tabla users en postgres.
    
    La migración permite crear la tabla en cualquier sistema de base de datos que soporte (SQLite, MySQL, etc...).

--- db/migrations/20200607121313_user.ts
+++ db/migrations/20200607121313_user.ts
@@ -1,11 +1,15 @@
 import * as Knex from "knex";
 
-
 export async function up(knex: Knex): Promise<any> {
-    
+	return Promise.all([
+		knex.schema.createTable("users", (table) => {
+			table.increments("id").primary();
+			table.string("email").unique().notNullable();
+			table.string("name").notNullable();
+		}),
+	]);
 }
-
 
 export async function down(knex: Knex): Promise<any> {
+	return Promise.all([knex.schema.dropTable("users")]);
 }
-
